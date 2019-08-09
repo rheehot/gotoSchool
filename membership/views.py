164ecacdefb10ad50f6842df74b19ce80716e057
Juhyeon <kjh96905@gmail.com>
2019-08-09 11:44:26 +0900

    edit membership/views.py

--- membership/views.py
+++ membership/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, render_to_response
 from django.http import HttpResponse
 from .forms import CreateUserForm, LoginForm, DeleteAccountForm, ChangePasswordForm
 from .models import Member
@@ -11,22 +11,24 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.hashers import check_password
 from django.template import RequestContext
-from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
     return render(request,  '../index.html')
 
 
+@csrf_exempt
 def signup(request):
     if request.method == 'POST':
         form = CreateUserForm(request.POST, request.FILES)
 
         if form.is_valid():
+            
             if form.cleaned_data['password'] == form.cleaned_data['passwordCheck']:
                 username = form.cleaned_data.get("username")
                 password = form.cleaned_data.get("password")
-
+                
                 newUser = form.save(commit=False)
                 newUser = Member.objects.create_user(username=username, email=None, password=password)
                 
@@ -38,15 +40,15 @@ def signup(request):
                     
                 newUser.save()
                 form.save_m2m()
-                messages.info('회원이 되어주셔서 감사합니다. WelCome ~ :)')
 
                 login(request, newUser)
+                messages.info(request, '회원이 되어주셔서 감사합니다. WelCome ~ :)')
                 return redirect('mypage')
             else:
                 return render(request, 'signup.html', {'form': form, 'error': '비밀번호를 다시 확인해주십시오.'}, context_instance=RequestContext(request))
         
         else:
-            messages.error('이미 존재하는 아이디이거나 회원가입에 실패했습니다. 다시 입력해주세요 :)')
+            messages.error(request, '이미 존재하는 아이디이거나 회원가입에 실패했습니다. 다시 입력해주세요 :)')
             return redirect('signup')
     
     else:
@@ -66,8 +68,7 @@ def signin(request):
             return redirect('home')
         
         else: 
-            messages.error('로그인에 실패했습니다. 다시 시도해주세요 :)')
-            return redirect('signin')
+            return render_to_response('login.html', {'message': '로그인에 실패했습니다. 다시 시도해주세요 :(', 'form': form})
     
     else:
         form = LoginForm()
@@ -94,16 +95,10 @@ def deleteAccount(request):
                 if user is not None:
                     user.delete()
                     return redirect('signin')
-                else:
-                    messages.error('회원 탈퇴에 실패했습니다. 다시 시도해주세요. ')
-                    return redirect('deleteAccount')
-
-        except user.DoesNotExist:
-            messages.error(request, "User does not exist")
-            return render(request, 'home.html')
 
         except Exception as e:
-            return render(request, 'login.html', {'err': print(e)})
+            messages.error(request, '아이디를 잘못 입력하셨습니다. 회원 탈퇴에 실패했습니다.')
+            return redirect('deleteAccount')
     else:
         form = DeleteAccountForm()
         return render(request, 'deleteAccount.html', {'form': form})
